834eb8401fb569666527eb54a749995e598b5a6d
taeroi <fkrkdlsgk@gmail.com>
2021-12-20 12:08:07 +0900

    feat: My_Story Model, 목데이터 추가

---
+++ TestDreamUI/My/My_MockDatas.swift
@@ -0,0 +1,24 @@
+//
+//  My_MockDatas.swift
+//  TestDreamUI
+//
+//  Created by 박태환 on 2021/12/20.
+//
+
+import Foundation
+
+
+let MyStoryDataResource: [MyStoryModel] = [
+    MyStoryModel(
+        coverImageName: "my_sample_image2",
+        title: "sample 1"
+    ),
+    MyStoryModel(
+        coverImageName: "my_sample_image3",
+        title: "sample 2"
+    ),
+    MyStoryModel(
+        coverImageName: "my_sample_image4",
+        title: "sample 3"
+    )
+]
---
+++ TestDreamUI/My/My_StoryModel.swift
@@ -0,0 +1,13 @@
+//
+//  My_StoryModel.swift
+//  TestDreamUI
+//
+//  Created by 박태환 on 2021/12/20.
+//
+
+import Foundation
+
+struct MyStoryModel {
+    let coverImageName: String
+    let title: String
+}
