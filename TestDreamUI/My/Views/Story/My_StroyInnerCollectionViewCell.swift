d0bcb94a19e18f5079840c6760f50b64b7aa0575
taeroi <fkrkdlsgk@gmail.com>
2021-12-20 12:12:52 +0900

    feat: MyStroy 내부 뷰 생성

---
+++ TestDreamUI/My/Views/Story/My_StroyInnerCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  My_StroyInnerCollectionViewCell.swift
+//  TestDreamUI
+//
+//  Created by 박태환 on 2021/12/20.
+//
+
+import UIKit
+
+final class MyStroyInnerCollectionViewCell: BaseCollectionViewCell {
+    
+}
